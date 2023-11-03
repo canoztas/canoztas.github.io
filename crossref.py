d1fcb9f3bf1a1217baf1054d8dac477a71a25818
canoztas <59293768+canoztas@users.noreply.github.com>
2023-11-03 09:07:51 +0300

    Update crossref.py

--- crossref.py
+++ crossref.py
@@ -24,7 +24,7 @@ main_df.to_excel('crossref'+str(PAGE)+'.xlsx',index=True,startrow=0)
 for b in brands["results"]:
     if b['id']=='f865c8b7-d2c2-41ce-896b-bc4d3a31a4e0':
         continue
-    iterator=2
+    iterator=1
     while True:
         response = requests.get('https://catalog.hifi-filter.com/api/cross-reference/brand/'+b['id']+'/reference?p='+str(iterator))
         cr = response.json()
